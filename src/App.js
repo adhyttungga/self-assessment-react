f458180d571764f9e9eab6d5d44036c2b406c7c0
cap7_nemo <adhyttungga.jkt@gmail.com>
2022-11-14 21:14:12 +0700

    update file app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Suspense } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
 
 const loading = (
@@ -9,15 +9,8 @@ const loading = (
 )
 
 // Containers
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
-
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
-const SelfAssessment = React.lazy(() => import('./views/self-assessment/SelfAssessment'))
-const Pertanyaan = React.lazy(() => import('./views/pertanyaan/Pertanyaan'))
+const SALayout = React.lazy(() => import('./layout/SALayout'))
+const PertanyaanLayout = React.lazy(() => import('./layout/PertanyaanLayout'))
 
 class App extends Component {
   render() {
@@ -25,18 +18,13 @@ class App extends Component {
       <HashRouter>
         <Suspense fallback={loading}>
           <Routes>
+            <Route path="/self-assessment/*" name="Self Assessment" element={<SALayout />} />
             <Route
-              exact
-              path="/self-assessment"
-              name="Self Assessment"
-              element={<SelfAssessment />}
+              path="/self-assessment-soal/*"
+              name="Self Assessment Soal"
+              element={<PertanyaanLayout />}
             />
-            <Route exact path="/pertanyaan" name="Pertanyaan" element={<Pertanyaan />} />
-            <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route path="*" name="Home" element={<DefaultLayout />} />
+            <Route path="/" element={<Navigate to="/self-assessment" replace />} />
           </Routes>
         </Suspense>
       </HashRouter>
