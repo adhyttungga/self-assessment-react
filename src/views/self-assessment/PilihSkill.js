3a90c51db73dea3ec5a6c95f22d39da2e1a3ba3c
cap7_nemo <adhyttungga.jkt@gmail.com>
2022-11-14 21:17:48 +0700

    update pilih skill page

--- src/views/self-assessment/PilihSkill.js
+++ src/views/self-assessment/PilihSkill.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { CRow, CCol, CButton } from '@coreui/react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const PilihSkill = () => {
+  const navigate = useNavigate()
   const selectedSkillList = useSelector((state) => state.selectedSkillList)
   const selectedSkill = useSelector((state) => state.selectedSkill)
   const dispatch = useDispatch()
@@ -21,16 +23,17 @@ const PilihSkill = () => {
   }
 
   const handleBack = () => {
-    dispatch({ type: 'set', selectedSkillList: [], selectedSkill: [] })
+    dispatch({ type: 'set', selectedSkillList: [], selectedSkill: [], activeKey: 3 })
+    navigate('/self-assessment/pilih-role')
+  }
+
+  const handleSubmit = () => {
+    dispatch({ type: 'set', jawabanSA1: [] })
+    navigate('/self-assessment-soal/self-assessment-1')
   }
 
   return (
     <>
-      {console.log(selectedSkill)}
-      <h2>Pilih Skill Anda</h2>
-
-      <p> Pilih skill yang anda kuasai (minimal basic knowledge)</p>
-
       <div className="mb-3">
         {skillList &&
           skillList.map((item, i) => (
@@ -53,7 +56,7 @@ const PilihSkill = () => {
         </CCol>
 
         <CCol md={8} className="d-grid mb-2">
-          <CButton color="primary" size="lg">
+          <CButton color="primary" size="lg" onClick={handleSubmit}>
             Submit & Lanjut ke Pengisian Assessment
           </CButton>
         </CCol>
