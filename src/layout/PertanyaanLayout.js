caee575421001048933f5a761184162f2547c6d9
cap7_nemo <adhyttungga.jkt@gmail.com>
2022-11-15 08:38:27 +0700

    clean console

--- src/layout/PertanyaanLayout.js
+++ src/layout/PertanyaanLayout.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { PertanyaanContent, PertanyaanHeader } from '../components/index'
-import { useSelector, useDispatch } from 'react-redux'
 
 const PertanyaanLayout = () => {
   return (
     <div>
-      {console.log(localStorage.getItem('formDiri'))}
       <div className="wrapper d-flex flex-column min-vh-100 bg-white">
         <PertanyaanHeader />
         <div className="body flex-grow-1 px-3">
