b7322746cd239c7b4f32407fc73cc86ff6780967
cap7_nemo <adhyttungga.jkt@gmail.com>
2023-01-03 17:19:26 +0700

    modified informasi skill

--- src/views/pertanyaan/InformasiSkill.js
+++ src/views/pertanyaan/InformasiSkill.js
@@ -9,34 +9,66 @@ import {
   CTableHeaderCell,
   CTableRow,
 } from '@coreui/react'
+import { useSelector } from 'react-redux'
 
 import informasiSkill from './_iskill'
 
 const InformasiSkill = () => {
+  const keywordInfoSkill = useSelector((state) => state.keywordInfoSkill)
+
+  const [iSkill, setISkill] = React.useState(informasiSkill)
+
+  React.useEffect(() => {
+    const filteredSkill = informasiSkill.filter((skill) =>
+      skill?.skill?.toLowerCase()?.includes(keywordInfoSkill.toLowerCase()),
+    )
+
+    setISkill(filteredSkill)
+  }, [keywordInfoSkill])
   return (
     <CCard className="mb-4 border-0">
       <CCardBody className="p-0">
         <CTable>
           <CTableHead className="align-middle">
             <CTableRow>
-              <CTableHeaderCell style={{ width: '10%' }} scope="col">
+              <CTableHeaderCell
+                className="custom-btn-next text-white"
+                style={{
+                  width: '10%',
+                  borderTopLeftRadius: 'var(--cui-border-radius)',
+                  whiteSpace: 'nowrap',
+                }}
+                scope="col"
+              >
                 No
               </CTableHeaderCell>
 
-              <CTableHeaderCell style={{ width: '30%' }} scope="col">
+              <CTableHeaderCell
+                className="custom-btn-next text-white"
+                style={{ width: '30%', whiteSpace: 'nowrap' }}
+                scope="col"
+              >
                 Skill
               </CTableHeaderCell>
 
-              <CTableHeaderCell style={{ width: '60%' }} scope="col">
+              <CTableHeaderCell
+                className="custom-btn-next text-white"
+                style={{
+                  width: '60%',
+                  borderTopRightRadius: 'var(--cui-border-radius)',
+                  whiteSpace: 'nowrap',
+                }}
+                scope="col"
+              >
                 Deskripsi
               </CTableHeaderCell>
             </CTableRow>
           </CTableHead>
 
           <CTableBody>
-            {informasiSkill &&
-              informasiSkill.map((element, i) => (
-                <CTableRow key={`duj_list_${i}`}>
+            {iSkill &&
+              iSkill.map((element, i) => (
+                <CTableRow key={`info_skill_list${i}`}>
                   <CTableDataCell style={{ width: '10%' }}>{element.no}</CTableDataCell>
 
                   <CTableDataCell style={{ width: '30%' }}>{element.skill}</CTableDataCell>
