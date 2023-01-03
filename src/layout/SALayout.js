7c85b813dafbc4eaccc1497e74d2875713b11069
cap7_nemo <adhyttungga.jkt@gmail.com>
2023-01-03 17:19:13 +0700

    modified layout SA

--- src/layout/SALayout.js
+++ src/layout/SALayout.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { SAContent, SAHeader } from '../components/index'
+import { SAContent, SAHeader, TheSidebar } from '../components/index'
 import { useSelector, useDispatch } from 'react-redux'
 
 const SALayout = () => {
   return (
     <div>
-      <div className="wrapper d-flex flex-column min-vh-100 bg-white">
+      <TheSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-white sidebar-end">
         <SAHeader />
         <div className="body flex-grow-1 px-3">
           <SAContent />
